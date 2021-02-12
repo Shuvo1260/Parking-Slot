4c623d0284dfcaac13f3d542d8cc816b7f377098
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-02-13 05:14:36 +0600

    Booking cancel is implemented.

---
+++ lib/Utils/BookingManager.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:parking_slot/Resources/strings.dart';
+
+class BookingManager {
+  Future<bool> cancelBooking(id) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection(PATH_PARKING_DATA)
+          .doc(id)
+          .delete();
+      return true;
+    } catch (error) {
+      print("BookingCancelError: $error");
+      return false;
+    }
+  }
+}
